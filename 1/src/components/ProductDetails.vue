6900e12b62ab29f7075d073734223e125c54f8b3
Mary <mariavnuzhdina@yandex.ru>
2024-03-26 19:39:41 +0300

    Homework_5

---
+++ 1/src/components/ProductDetails.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <div v-for="product in products" :key="product.name" class="product">
+            <img :src="product.img" alt="">
+            <h1>Наименование товара: </h1>
+            <p>{{ product.name }}</p>
+            <h2 v-if="product.available">Available</h2>
+            <h2 v-else>Out of Stock</h2>
+            <h2>Цена: {{ formattedPrice(product) }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductDetails',
+
+    data() {
+        return {
+            products: [
+                { img: require('../assets/phone.png'), name: 'phone', price: 100, available: false },
+                { img: require('../assets/mouse.jpg'), name: 'mouse', price: 50, available: true },
+            ]
+        }
+    },
+
+    computed: {
+        formattedPrice() {
+            return function (product) {
+                return '$' + product.price.toFixed(2);
+            };
+        }
+    },
+
+    mounted() {
+
+    },
+}
+</script>
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+}
+
+.product {
+    border: 1px solid #010000;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+img {
+    width: 30%;
+    margin: 40px;
+    padding: 15px;
+}
+</style>
---
+++ 2/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <HeaderComponent></HeaderComponent>
+    <div class="top-banner">
+      <section class="blog-details__intro">
+        <img src="./assets/blog-details-intro.png" alt="photo">
+      </section>
+    </div>
+    <div id="blog-details" class="blog-details center">
+      <blogsDetails></blogsDetails>
+    </div>
+    <FooterComponent></FooterComponent>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import blogsDetails from './components/blogsDetails.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    blogsDetails,
+    HeaderComponent,
+    FooterComponent,
+
+  }
+}
+</script>
+
+<style scoped>
+.top-banner {
+  margin-bottom: 50px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  min-height: 351px;
+  max-width: 500px;
+}
+</style>
